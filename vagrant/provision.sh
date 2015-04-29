16b2bc1482cb156e0bc402c330eaba37d2e65aaf
L. Guruprasad <lgp171188@gmail.com>
2015-04-29 16:45:46 +0530

    Create the basic project structure

---
+++ vagrant/provision.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cd ~vagrant/sweet_life
+
+apt-get update
+
+export LC_ALL="en_US.UTF-8"
+locale-gen en_US.UTF-8
+
+apt-get upgrade -y -q
+
+apt-get install -y -q npm
+
+ln -sf /usr/bin/nodejs /usr/local/bin/node
+
+npm install
+
+apt-get install -y -q python-virtualenv libpython-dev
+apt-get install -y -q git
+
+VENV=/home/vagrant/.virtualenvs/sweet_life
+
+sudo -H -u vagrant virtualenv $VENV
+
+sudo -H -u vagrant -s -- <<EOF
+source $VENV/bin/activate
+cd ~/sweet_life
+pip install -r requirements/requirements.txt
+pip install -r requirements/dev.txt
+pre-commit install -f
+EOF
+
+apt-get autoremove -y -q
+
+echo ". $VENV/bin/activate" >> /home/vagrant/.bashrc
+
+echo "export PATH=\$PATH:/home/vagrant/sweet_life/node_modules/.bin" >> /home/vagrant/.bashrc
+
+cat <<EOF
+********************************************************************************
+
+VM provisioning is done
+
+********************************************************************************
+EOF
